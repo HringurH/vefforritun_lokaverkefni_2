df42034245f7ce311be8b7294af07cc8f23bdef9
HringurH <hringur.hrafnkelsson@gmail.com>
2026-03-03 10:44:49 +0000

    Added inital files

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.set('views', path.join(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Recipes' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log('Press CTRL+C to stop the server');
+});
