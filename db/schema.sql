5adbaaf819471539e0bd2f08d6b60b81dc77b2ad
HringurH <hringur.hrafnkelsson@gmail.com>
2026-03-13 10:41:53 +0000

    Byrjaði á gagnagrunni

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+CREATE TABLE IF NOT EXISTS recipes (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+    work_level INT CHECK (work_level >= 1 AND work_level <= 5),
+    time_seconds INT NOT NULL,
+    description TEXT NOT NULL,
+    image_path VARCHAR(255)
+)
+
+CREATE TABLE IF NOT EXISTS ingredients (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+)
+
+CREATE TABLE recipe_ingredients (
+    recipe_id INT,
+    ingredient_id INT,
+    quantity INT,
+
+    PRIMARY KEY(recipe_id, ingredient_id),
+    CONSTRAINT fk_ri_recipe
+        FOREIGN KEY (recipe_id)
+        REFERENCES recipes(id)
+        ON DELETE CASCADE,
+    CONSTRAINT fk_ri_ingredients
+        FOREIGN KEY (ingredient_id)
+        REFERENCES ingredients(id)
+        ON DELETE CASCADE
+)
