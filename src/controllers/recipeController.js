8ea5925ab309fe80406d2de7f64507343afd0547
HringurH <hringur.hrafnkelsson@gmail.com>
2026-04-10 10:12:08 +0000

    Created recipe details page and supporting structure

--- src/controllers/recipeController.js
+++ src/controllers/recipeController.js
@@ -14,6 +14,26 @@ const getHomePage = async (req, res) => {
     }
 };
 
+const getRecipeDetails = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const recipe = await recipeService.getRecipeById(id);
+
+        if (!recipe) {
+            return res.status(404).render(('404', {title: 'Recipe Not Found (404)'}))
+        }
+
+        res.render('recipe-details', {
+            title: recipe.name,
+            recipe: recipe
+        });
+    } catch (error) {
+        console.error('Error fetching recipe', error);
+        res.status(500).render('500', {title: 'Internal Server Error (500)'});
+    }
+}
+
 module.exports = {
-    getHomePage
+    getHomePage,
+    getRecipeDetails
 };
--- src/routes/recipeRoutes.js
+++ src/routes/recipeRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const recipeController = require('../controllers/recipeController')
 
 router.get('/', recipeController.getHomePage);
+router.get('/recipe/:id', recipeController.getRecipeDetails);
 
 module.exports = router;
